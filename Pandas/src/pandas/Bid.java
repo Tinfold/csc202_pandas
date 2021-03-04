a24445c892b5852e0c11cf77bdb3f1528b9ba396
SintaClaws <78180937+SintaClaws@users.noreply.github.com>
2021-03-04 17:33:00 -0500

    Create Bid.java

---
+++ Pandas/src/pandas/Bid.java
@@ -0,0 +1,56 @@
+package pandas;
+
+import java.text.NumberFormat;
+
+public class Bid {
+
+	private Customer cust;
+	private double bid;
+	private double maxBid;
+	
+	
+	public Bid() {
+		
+	}
+	
+	public Bid(Customer cust, double bid, double maxBid) {
+		this.cust = cust;
+		this.bid = bid;
+		this.maxBid = maxBid;			
+	}
+
+	public void setCust(Customer cust) {
+		this.cust = cust;
+	}
+
+	public Customer getCust() {
+		return cust;
+	}
+
+	public void setId(Customer cust) {
+		this.cust = cust;
+	}
+
+	public double getBid() {
+		return bid;
+	}
+
+	public void setBid(double bid) {
+		this.bid = bid;
+	}
+
+	public double getMaxBid() {
+		return maxBid;
+	}
+
+	public void setMaxBid(double maxBid) {
+		this.maxBid = maxBid;
+	}
+	
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance();
+		return cust.toString() + " || " + nf.format(bid);
+	}
+	
+	
+}
